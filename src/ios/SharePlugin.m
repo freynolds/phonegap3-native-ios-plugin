676bf9dd5c7b30add411cd181b987f1fd51510e6
Francisco Reynolds <francisco@iamat.com>
2015-04-10 13:08:49 -0300

    [whatsapp] & updates

--- src/ios/SharePlugin.m
+++ src/ios/SharePlugin.m
@@ -17,9 +17,10 @@
     
     self_arguments = [[NSMutableArray alloc]initWithArray:command.arguments];
     
-    UIActionSheet *popup = [[UIActionSheet alloc] initWithTitle:@"Share" delegate:self cancelButtonTitle:@"Cancelar" destructiveButtonTitle:nil otherButtonTitles:
+    UIActionSheet *popup = [[UIActionSheet alloc] initWithTitle:@"Compartir en:" delegate:self cancelButtonTitle:@"Cancelar" destructiveButtonTitle:nil otherButtonTitles:
                             @"Facebook",
                             @"Twitter",
+                            @"WhatsApp",
                             nil];
     
     [popup showInView:self.viewController.view];
@@ -28,126 +29,148 @@
 
 - (void)actionSheet:(UIActionSheet *)popup clickedButtonAtIndex:(NSInteger)buttonIndex
 {
-
+    
     NSString *text = [[self_arguments objectAtIndex:0] objectForKey:@"text"];
     NSString *urlAttach = [[self_arguments objectAtIndex:0] objectForKey:@"urlAttach"];
     NSString *imageAttach = [[self_arguments objectAtIndex:0] objectForKey:@"imageAttach"];
     
     switch (buttonIndex) {
         case 0: {
-                    NSLog(@"Facebook");
-                    dispatch_async(dispatch_get_main_queue(), ^ {
-        
-        				SLComposeViewController *fbController=[SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
-        
-        
-        		        SLComposeViewControllerCompletionHandler __block completionHandler=^(SLComposeViewControllerResult result){
-        
-        		            [fbController dismissViewControllerAnimated:YES completion:nil];
-        
-        		            switch(result){
-        		                case SLComposeViewControllerResultCancelled:
-        		                default:
-        		                {
-        		                    NSLog(@"Cancelled.....");
-        
-        		                }
-        		                    break;
-        		                case SLComposeViewControllerResultDone:
-        		                {
-        		                    NSLog(@"Posted....");
-        		                }
-        		                    break;
-        		            }};
-        		        [fbController setInitialText:text];
-        		        if(imageAttach != nil){
-        		            [fbController addImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageAttach]]]];
-        		        }
-        		        if(urlAttach != nil && imageAttach == nil){
-        		            [fbController addURL:[NSURL URLWithString:urlAttach]];
-        		        }
-        		        [fbController setCompletionHandler:completionHandler];
-        		        [self.viewController presentViewController:fbController animated:YES completion:nil];
-                    });
-        
-                    break;
+            NSLog(@"Facebook");
+            dispatch_async(dispatch_get_main_queue(), ^ {
+                
+                SLComposeViewController *fbController=[SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+                
+                
+                SLComposeViewControllerCompletionHandler __block completionHandler=^(SLComposeViewControllerResult result){
+                    
+                    [fbController dismissViewControllerAnimated:YES completion:nil];
+                    
+                    switch(result){
+                        case SLComposeViewControllerResultCancelled:
+                        default:
+                        {
+                            NSLog(@"Cancelled.....");
+                            
+                        }
+                            break;
+                        case SLComposeViewControllerResultDone:
+                        {
+                            NSLog(@"Posted....");
+                        }
+                            break;
+                    }};
+                [fbController setInitialText:text];
+                if(imageAttach != nil){
+                    [fbController addImage:[UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageAttach]]]];
                 }
+                if(urlAttach != nil && imageAttach == nil){
+                    [fbController addURL:[NSURL URLWithString:urlAttach]];
+                }
+                [fbController setCompletionHandler:completionHandler];
+                [self.viewController presentViewController:fbController animated:YES completion:nil];
+            });
+            
+            break;
+        }
         case 1: {
-                    NSLog(@"Twitter");
-                    dispatch_async(dispatch_get_main_queue(), ^ {
-
-                        SLComposeViewController *tweetViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
-                        
-                        SLComposeViewControllerCompletionHandler __block completionHandler=^(SLComposeViewControllerResult result){
+            NSLog(@"Twitter");
+            dispatch_async(dispatch_get_main_queue(), ^ {
+                
+                SLComposeViewController *tweetViewController = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+                
+                SLComposeViewControllerCompletionHandler __block completionHandler=^(SLComposeViewControllerResult result){
+                    
+                    [tweetViewController dismissViewControllerAnimated:YES completion:nil];
+                    
+                    switch(result){
+                        case SLComposeViewControllerResultCancelled:
+                        default:
+                        {
+                            NSLog(@"Cancelled.....");
                             
-                            [tweetViewController dismissViewControllerAnimated:YES completion:nil];
-                            
-                            switch(result){
-                                case SLComposeViewControllerResultCancelled:
-                                default:
-                                {
-                                    NSLog(@"Cancelled.....");
-                                    
-                                }
-                                    break;
-                                case SLComposeViewControllerResultDone:
-                                {
-                                    NSLog(@"Posted....");
-                                }
-                                    break;
-                            }};
-
-
-					    BOOL ok = YES;
-					    NSString *errorMessage;
-
-					    if(text != nil){
-					        ok = [tweetViewController setInitialText:text];
-					        if(!ok){
-					            errorMessage = @"Tweet is too long";
-					        }
-					    }
-
-
-
-					    if(imageAttach != nil){
-					        // Note that the image is loaded syncronously
-					        if([imageAttach hasPrefix:@"http://"]){
-					            UIImage *img = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageAttach]]];
-					            ok = [tweetViewController addImage:img];
-					        }
-					        else{
-					            ok = [tweetViewController addImage:[UIImage imageNamed:imageAttach]];
-					        }
-					        if(!ok){
-					            errorMessage = @"Image could not be added";
-					        }
-					    }
-
-						if(urlAttach != nil){
-					        ok = [tweetViewController addURL:[NSURL URLWithString:urlAttach]];
-					        if(!ok){
-					            errorMessage = @"URL too long";
-					        }
-					    }
-
-
-
-					    if(!ok){
-                            NSLog(@"%@",errorMessage);
-					    }
-					    else{
-
-                            [tweetViewController setCompletionHandler:completionHandler];
-                            [self.viewController presentViewController:tweetViewController animated:YES completion:nil];
-					    }
-
-                    });
-                    break;
+                        }
+                            break;
+                        case SLComposeViewControllerResultDone:
+                        {
+                            NSLog(@"Posted....");
+                        }
+                            break;
+                    }};
+                
+                
+                BOOL ok = YES;
+                NSString *errorMessage;
+                
+                if(text != nil){
+                    ok = [tweetViewController setInitialText:text];
+                    if(!ok){
+                        errorMessage = @"Tweet is too long";
+                    }
                 }
+                
+                
+                
+                if(imageAttach != nil){
+                    // Note that the image is loaded syncronously
+                    if([imageAttach hasPrefix:@"http://"]){
+                        UIImage *img = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageAttach]]];
+                        ok = [tweetViewController addImage:img];
+                    }
+                    else{
+                        ok = [tweetViewController addImage:[UIImage imageNamed:imageAttach]];
+                    }
+                    if(!ok){
+                        errorMessage = @"Image could not be added";
+                    }
+                }
+                
+                if(urlAttach != nil){
+                    ok = [tweetViewController addURL:[NSURL URLWithString:urlAttach]];
+                    if(!ok){
+                        errorMessage = @"URL too long";
+                    }
+                }
+                
+                
+                
+                if(!ok){
+                    NSLog(@"%@",errorMessage);
+                }
+                else{
+                    
+                    [tweetViewController setCompletionHandler:completionHandler];
+                    [self.viewController presentViewController:tweetViewController animated:YES completion:nil];
+                }
+                
+            });
+            break;
+        }
+        case 2: {
+            NSLog(@"Whatsapp");
+            
+            NSString *unescapedString = [NSString stringWithFormat:@"%@ %@",text, urlAttach];
+            
+            NSString *escapedString = (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(
+                                                                                                            NULL,
+                                                                                                            (CFStringRef)unescapedString,
+                                                                                                            NULL,
+                                                                                                            CFSTR("!*'();:@&=+$,/?%#[]\" "),
+                                                                                                            kCFStringEncodingUTF8));
+            
+            NSString *whastappURLString = [@"whatsapp://send?text=" stringByAppendingString:escapedString];
+            
+            NSURL *whatsappURL = [NSURL URLWithString:whastappURLString];
+            
+            
+            if ([[UIApplication sharedApplication] canOpenURL: whatsappURL]) {
+                [[UIApplication sharedApplication] openURL: whatsappURL];
+            }
+        }
         default:
             break;
     }
 }
+
 
 @end
